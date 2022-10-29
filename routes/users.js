1b2b690058a34a6ad50f9412639de2fe8d258796
WyneHtet <wynehtet2022@gmail.com>
2022-10-29 11:22:08 +0630

    email send and agent change

--- routes/users.js
+++ routes/users.js
@@ -1,59 +1,49 @@
-var express = require("express");
-var router = express.Router();
-var User = require("../models/nUsers");
-var Agent = require("../models/aUsers");
-var Swal = require("sweetalert2");
+var express = require("express")
+var router = express.Router()
+var User = require("../models/nUsers")
+var Agent = require("../models/aUsers")
+var Swal = require("sweetalert2")
 // var transporter = require("../models/emailVerification");
-var crypto = require("crypto");
-var cookie = require("cookie-parser");
-var jwt = require("jsonwebtoken");
-var nodemailer = require("nodemailer");
-var randomString = require("randomstring");
-const SMTPConnection = require("nodemailer/lib/smtp-connection");
-var config = require("../config/config");
+var crypto = require("crypto")
+var cookie = require("cookie-parser")
+var jwt = require("jsonwebtoken")
+var nodemailer = require("nodemailer")
+var randomString = require("randomstring")
+const SMTPConnection = require("nodemailer/lib/smtp-connection")
+// var config = require("../config/config")
 
-var sendResetPasswordMail = require("../models/emailVerification");
+var sendResetPasswordMail = require("../models/emailVerification")
 
-var Post = require("../models/agent-postadd");
-var multer = require("multer");
-var upload = multer({ dest: "public/images/testimonials" });
+var Post = require("../models/agent-postadd")
+var multer = require("multer")
+var upload = multer({ dest: "public/images/testimonials" })
 
-var dotenv = require("dotenv");
-dotenv.config();
-console.log(
-  process.env.USER_EMAIL,
-  process.env.PASSWORD,
-  typeof process.env.USER_EMAIL,
-  typeof process.env.PASSWORD
-);
+var dotenv = require("dotenv")
+dotenv.config()
+// console.log(
+//   process.env.USER_EMAIL,
+//   process.env.PASSWORD,
+//   typeof process.env.USER_EMAIL,
+//   typeof process.env.PASSWORD
+// )
 const agentAuth = function (req, res, next) {
   if (req.session.agent) {
-    next();
+    next()
   } else {
-    res.redirect("/users/agentLogin");
+    res.redirect("/users/agentLogin")
   }
-};
+}
 const userAuth = function (req, res, next) {
   if (req.session.user) {
-    next();
+    next()
   } else {
-    res.redirect("/users/nlogin");
+    res.redirect("/users/nlogin")
   }
-};
-
-//nodemailer
-// var transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: "travelbagproject30@gmail.com",
-//     pass: "travel#2022bag",
-//   },
-// });
-
+}
 /* GET users listing. */
 router.get("/", userAuth, function (req, res, next) {
-  res.redirect("/users/dashboard");
-});
+  res.redirect("/users/dashboard")
+})
 
 // for send verify mail
 const sendVerifyMail = async (normalName, normalEmail, User_id) => {
@@ -71,88 +61,111 @@ const sendVerifyMail = async (normalName, normalEmail, User_id) => {
       tls: {
         ciphers: "SSLv3",
       },
-    });
+    })
 
     const mailOptions = {
-      from: config.UserEmail,
+      from: "Travel Bag<process.env.USER_EMAIL>",
       to: normalEmail,
       subject: "For Email Verification",
       html:
         "<p>Hi " +
         normalName +
-        ', Please click here to <a href="https://127.0.0.1:4000/users/verify?verify=' +
+        ', Please click here to <a href="https://127.0.0.1:4000/users/verify?id=' +
         User_id +
         '">verification your mail.</a></p>',
-    };
+    }
 
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
-        console.log(error);
+        console.log(error)
       } else {
-        console.log("Email has been sent:- ", info.response);
+        console.log("Email has been sent:- ", info.response)
       }
-    });
+    })
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message)
   }
-};
+}
 
 //normal users sign up
 router.get("/nsignup", function (req, res) {
-  res.render("users/normalUsers/nuserSignUp");
-});
+  res.render("users/normalUsers/nuserSignUp")
+})
 
 //normal users sign up data
 router.post("/nsignup", function (req, res) {
   try {
-    const { normalName, normalEmail, normalPassword } = req.body;
+    const { normalName, normalEmail, normalPassword } = req.body
     var user = new User({
       normalName,
       normalEmail,
       normalPassword,
-      normalemailToken: crypto.randomBytes(64).toString("hex"),
       normalisVerified: false,
-    });
+    })
 
-    const newUser = user.save();
+    const newUser = user.save()
 
     if (newUser) {
-      sendVerifyMail(req.body.normalName, req.body.normalEmail, user._id);
+      sendVerifyMail(req.body.normalName, req.body.normalEmail, user._id)
       res.render("users/normalUsers/nuserSignUp", {
         message:
-          "အကောင့် ပြုလုပ်ခြင်းအောင်မြင်ပါသည် ။ သင့် အီးမေးလ် သို့အတည်ပြုမေးလ် ပို့ပေးထားပါသည် ။ အတည်ပြု၍  အကောင့်ဝင်ပါ ။",
-      });
+          "အကောင့် ပြုလုပ်ခြင်းအောင်မြင်ပါသည်။ သင့် အီးမေးလ် သို့အတည်ပြုမေးလ် ပို့ပေးထားပါသည်။ အတည်ပြု၍  အကောင့်ဝင်ပါ။",
+      })
     } else {
       res.render("users/normalUsers/nuserSignUp", {
-        message: "အကောင့်ဝင်ခြင်း မအောင်မြင်ပါ ။",
-      });
+        message: "အကောင့်ဝင်ခြင်း မအောင်မြင်ပါ။",
+      })
     }
-
-    // exports.contactForm = (req, res) => {
-    //   const { normalName, normalEmail } = req.body;
-    // };
 
     // res.redirect("/users/nlogin");
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
-router.get("/verify", function (req, res) {
-  res.render("users/normalUsers/verify");
-});
+// router.use(
+//   (verifyMail = async (req, res) => {
+//     try {
+//       const updateInfo = await User.updateOne(
+//         { _id: req.query.id },
+//         { $set: { normalisVerified: true } }
+//       )
+//       console.log("UpdateInfo :", updateInfo)
+//       res.render("users/normalUsers/verify")
+//     } catch (error) {
+//       console.log(error.message)
+//     }
+//   })
+// )
 
-router.post("/verify", async (req, res) => {
-  try {
-    const updateInfo = await User.updateOne(
-      { _id: req.query.id },
-      { $set: { normalisVerified: true } }
-    );
-    console.log(updateInfo);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+// router.get("/verify", verifyMail, function (req, res) {
+//   res.render("users/normalUsers/verify")
+// })
+
+// router.post("/verify", function(req, res){
+//   User.findOne({ normalEmail: req.body.normalEmail }, function (err, rtn) {
+//     if (err) throw err
+//     if (
+//       rtn.normalisVerified === true &&
+//       rtn != null &&
+//       User.compare(req.body.normalPassword, rtn.normalPassword)
+//       // User.compare(req.body.normalisVerified, rtn.normalisVerified)
+//     ) {
+//       //renember login
+//       req.session.user = {
+//         id: rtn._id,
+//         normalName: rtn.normalName,
+//         normalEmail: rtn.normalEmail,
+//       }
+//       res.redirect("/dashboard")
+//     }
+//      else {
+//       res.render("users/normalUsers/nuserLogin", {
+//         message: "တစ်စုံတစ်ရာ မှားယွင်းနေပါသည်။ အကောင့်ပြန်ဝင်ပါ ။",
+//       })
+//     }
+//   })
+// })
 
 // for resetPassword send mail
 
@@ -192,49 +205,52 @@ router.post("/verify", async (req, res) => {
 
 // normal users login
 router.get("/nlogin", function (req, res) {
-  res.render("users/normalUsers/nuserLogin");
-});
+  res.render("users/normalUsers/nuserLogin")
+})
 
 // normal users login data
 router.post("/nlogin", function (req, res) {
-  const data = User.findOne(
-    { normalEmail: req.body.normalEmail },
-    function (err, rtn) {
-      if (err) throw err;
-      if (
-        rtn != null &&
-        User.compare(req.body.normalPassword, rtn.normalPassword)
-      ) {
-        //renember login
-        req.session.user = {
-          id: rtn._id,
-          normalName: rtn.normalName,
-          normalEmail: rtn.normalEmail,
-        };
-        res.redirect("/dashboard");
-      } else {
-        res.render("users/normalUsers/nuserLogin", {
-          message: "တစ်စုံတစ်ရာ မှားယွင်းနေပါသည် ။ အကောင့်ပြန်ဝင်ပါ ။",
-        });
+  User.findOne({ normalEmail: req.body.normalEmail }, function (err, rtn) {
+    if (err) throw err
+    if (
+      rtn.normalisVerified === true &&
+      rtn != null &&
+      User.compare(req.body.normalPassword, rtn.normalPassword)
+      // User.compare(req.body.normalisVerified, rtn.normalisVerified)
+    ) {
+      //renember login
+      req.session.user = {
+        id: rtn._id,
+        normalName: rtn.normalName,
+        normalEmail: rtn.normalEmail,
       }
+      res.redirect("/dashboard")
+    } else if (rtn.normalisVerified === false) {
+      res.render("users/normalUsers/nuserLogin", {
+        message: "ကျေးဇူးပြု၍ သင့် အီးမေးလ် အတည်ပြုပါ။",
+      })
+    } else {
+      res.render("users/normalUsers/nuserLogin", {
+        message: "တစ်စုံတစ်ရာ မှားယွင်းနေပါသည်။ အကောင့်ပြန်ဝင်ပါ ။",
+      })
     }
-  );
-});
+  })
+})
 
 // normal user profile
 router.get("/nprofile", userAuth, function (req, res) {
-  res.render("users/normalUsers/nuser-profile.ejs");
-});
+  res.render("users/normalUsers/nuser-profile.ejs")
+})
 
 // normal user reset password
 router.get("/forget-password", function (req, res) {
-  res.render("users/normalUsers/resetPassword");
-});
+  res.render("users/normalUsers/resetPassword")
+})
 
 // normal user forget password
 router.get("/nforgetpassword", function (req, res) {
-  res.render("users/normalUsers/nUserforgotPassword");
-});
+  res.render("users/normalUsers/nUserforgotPassword")
+})
 
 // router.get("/nforgetpassword", forgetLoad);
 
@@ -242,73 +258,73 @@ router.get("/nforgetpassword", function (req, res) {
 
 router.post("/nforgetpassword", async (req, res) => {
   try {
-    const normalEmail = req.body.normalEmail;
-    console.log("User Input Data :", normalEmail);
+    const normalEmail = req.body.normalEmail
+    console.log("User Input Data :", normalEmail)
     User.findOne({ normalEmail: normalEmail }, (err, rtn) => {
-      if (err) throw err;
-      console.log("UserDate :", rtn);
+      if (err) throw err
+      console.log("UserDate :", rtn)
       if (rtn != null) {
         if (rtn.normalisVerified === false) {
           res.render("users/normalUsers/resetPassword", {
-            message: "သင့် အီးမေးလ် အတည်ပြုပါ ။",
-          });
+            message: "သင့် အီးမေးလ် အတည်ပြုပါ။",
+          })
         } else {
-          const randomString = randomString.generate();
+          const randomString = randomString.generate()
           const updatedDate = User.updateOne(
             { normalEmail: normalEmail },
             { $set: { token: randomString } }
-          );
+          )
           // /nUserforgotPassword
-          sendResetPasswordMail(rtn.normalName, rtn.normalEmail, randomString);
+          sendResetPasswordMail(rtn.normalName, rtn.normalEmail, randomString)
           res.render("users/normalUsers/resetPassword", {
-            message: "သင့် အီးမေးလ် ကို ကျေးဇူးပြု၍စစ်ပေးပါ ။",
-          });
+            message: "သင့် အီးမေးလ် ကို ကျေးဇူးပြု၍စစ်ပေးပါ။",
+          })
         }
       } else {
         res.render("users/normalUsers/resetPassword", {
-          message: " သင့် အီးမေးလ် မှားနေပါသည် ။",
-        });
+          message: " သင့် အီးမေးလ် မှားနေပါသည်။",
+        })
       }
-    });
+    })
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message)
   }
-});
+})
 
 // agent sign up
 router.get("/agentSignup", function (req, res) {
-  res.render("users/agentUsers/agentSignup");
-});
+  res.render("users/agentUsers/agentSignup")
+})
 
 // agent sign up data
 router.post("/agentSignup", function (req, res) {
   try {
-    const { agentName, agentEmail, agentPassword } = req.body;
+    const { agentName, agentEmail, agentPassword } = req.body
     var user = new Agent({
       agentName,
       agentEmail,
       agentPassword,
       agentemailToken: crypto.randomBytes(64).toString("hex"),
       agentisVerified: false,
-    });
-    const newUser = user.save();
-    console.log("NewUser:", newUser);
+    })
+    const newUser = user.save()
+    console.log("NewUser:", newUser)
 
-    res.redirect("/users/agentLogin");
+    res.redirect("/users/agentLogin")
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
 // agent Login
 router.get("/agentLogin", function (req, res) {
-  res.render("users/agentUsers/agentLogin");
-});
+  res.render("users/agentUsers/agentLogin")
+})
 
 // agent login data
 router.post("/agentLogin", function (req, res) {
   Agent.findOne({ agentEmail: req.body.agentEmail }, function (err, rtn) {
-    if (err) throw err;
+    if (err) throw err
     if (
       rtn != null &&
       Agent.compare(req.body.agentPassword, rtn.agentPassword)
@@ -318,49 +334,54 @@ router.post("/agentLogin", function (req, res) {
         id: rtn._id,
         agentName: rtn.agentName,
         agentEmail: rtn.agentEmail,
-      };
-      res.redirect("/users/agentpage");
+      }
+      res.redirect("/users/agentpage")
     } else {
       res.render("users/agentUsers/agentLogin", {
-        message: "တစ်စုံတစ်ရာ မှားယွင်းနေပါသည် ။ အကောင့်ပြန်ဝင်ပါ ။",
-      });
+        message: "တစ်စုံတစ်ရာ မှားယွင်းနေပါသည်။ အကောင့်ပြန်ဝင်ပါ ။",
+      })
     }
-  });
-});
+  })
+})
 
 // agent index
 router.get("/agentpage", agentAuth, function (req, res) {
-  res.render("users/agentUsers/agentindex");
-});
+  res.render("users/agentUsers/agentindex")
+})
 
 // agent-post-detail
 router.get("/adetail", agentAuth, function (req, res) {
-  res.render("users/agentUsers/agent-post-details");
-});
+  res.render("users/agentUsers/agent-post-details")
+})
 
 // agent-profile-detail
 router.get("/aprofile", agentAuth, function (req, res) {
-  res.render("users/agentUsers/agent-profile-detail");
-});
+  res.render("users/agentUsers/agent-profile-detail")
+})
 
 //agent-post-list
 router.get("/apostlist", agentAuth, function (req, res) {
-  res.render("users/agentUsers/agent-post-list");
-});
+  res.render("users/agentUsers/agent-post-list")
+})
 
 // agent forget password
 router.get("/aforgetpassword", function (req, res) {
-  res.render("users/agentUsers/forget-password");
-});
+  res.render("users/agentUsers/forget-password")
+})
+
+// agent change password
+router.get("/agent-change-password", function (req, res) {
+  res.render("users/agentUsers/changePassword")
+})
 
 //logout
 
 router.get("/logout", function (req, res) {
   req.session.destroy(function (err) {
-    if (err) throw err;
-    res.redirect("/");
-  });
-});
+    if (err) throw err
+    res.redirect("/")
+  })
+})
 
 // check users name duplicate
 router.post("/checkname", function (req, res) {
@@ -369,14 +390,14 @@ router.post("/checkname", function (req, res) {
       res.json({
         message: "Internal server error",
         status: "error",
-      });
+      })
     } else {
       res.json({
         status: rtn != null ? "username have" : "username no have",
-      });
+      })
     }
-  });
-});
+  })
+})
 
 // check users emalil duplicate
 
@@ -386,14 +407,14 @@ router.post("/checknemail", function (req, res) {
       res.json({
         message: "Internal server error",
         status: "error",
-      });
+      })
     } else {
       res.json({
         status: rtn != null ? "have" : "no have",
-      });
+      })
     }
-  });
-});
+  })
+})
 
 // check agent email duplicate
 router.post("/checkagentemail", function (req, res) {
@@ -402,14 +423,14 @@ router.post("/checkagentemail", function (req, res) {
       res.json({
         message: "Internal server error",
         status: "error",
-      });
+      })
     } else {
       res.json({
         status: rtn != null ? "have" : "no have",
-      });
+      })
     }
-  });
-});
+  })
+})
 
 // check agent name duplicate
 router.post("/checkagentname", function (req, res) {
@@ -418,14 +439,14 @@ router.post("/checkagentname", function (req, res) {
       res.json({
         message: "Internal server error",
         status: "error",
-      });
+      })
     } else {
       res.json({
         status: rtn != null ? "already name" : "no have name",
-      });
+      })
     }
-  });
-});
+  })
+})
 
 // postadd get method
 // router.get("/agentpage#contact"),
@@ -438,20 +459,20 @@ router.post("/users/agentpage"),
   agentAuth,
   upload.single("image"),
   function (req, res) {
-    var post = new Post();
-    post.title = req.body.title;
-    post.place = req.body.place;
-    post.image = req.body.image;
-    post.author = req.session.user.id;
-    post.content = req.body.content;
-    post.created = Date.now();
-    if (req.file) post.image = "/images/portfolio/" + req.file.filename;
+    var post = new Post()
+    post.title = req.body.title
+    post.place = req.body.place
+    post.image = req.body.image
+    post.author = req.session.user.id
+    post.content = req.body.content
+    post.created = Date.now()
+    if (req.file) post.image = "/images/portfolio/" + req.file.filename
     post.save(function (err, rtn) {
-      if (err) throw err;
-      console.log(rtn);
-      res.redirect("/");
-    });
-  };
+      if (err) throw err
+      console.log(rtn)
+      res.redirect("/")
+    })
+  }
 
 //   router.get("/users/agentpage"),agentAuth,fuction(req,res){
 //     Post.find({author: req.session.user.id},function(err,rtn){
@@ -461,4 +482,4 @@ router.post("/users/agentpage"),
 //     })
 //
 
-module.exports = router;
+module.exports = router
